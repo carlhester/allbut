6bf0dd81cad4f4bc8d8966e3a7d9b2f1009ae5a7
Carl Hester <carlhester@gmail.com>
2022-01-27 09:09:25 -0800

    wip

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # allbut
+
+Delete all files in your current directory except the files you identify.  
+
+## Usage
+
+$ allbut file-to-protect
+
+- Given a current path with 4 files: testfile1, testfile2, testfile3, testfile4
+- Run _allbut testfile1_ to remove all of the files and leave only testfile1 in current path
+- Also works with multiple files _allbut testfile1 testfile2_
+- Normal operation just prints the work to do, the -f flag is required to actually delete
+
--- main.go
+++ main.go
@@ -1,67 +1,138 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/fs"
+	"io/ioutil"
+	"log"
 	"os"
 )
 
 func main() {
-	saveFile, force := parseArgs(os.Args)
-	fmt.Println(saveFile, force)
+	// Parse input
+	saveFiles, deletionEnabled := parseArgs(os.Args)
+
+	// validate input files
+	for _, saveFile := range saveFiles {
+		err := validateFile(saveFile)
+		if err != nil {
+			log.Printf("error validating file %s. err %v", saveFile, err)
+			printUsageAndExit()
+		}
+	}
+
+	// collect files from current directory
+	filesInCwd, err := ioutil.ReadDir("./")
+	if err != nil {
+		log.Printf("error reading directory. err %v", err)
+		printUsageAndExit()
+	}
+
+	// process files
+	deletionCandidates, err := identifyDeletionCandidates(saveFiles, filesInCwd)
+	if err != nil {
+		log.Print(err)
+		printUsageAndExit()
+	}
+
+	confirm, err := printStatusAndVerify(saveFiles, deletionCandidates)
+	if err != nil {
+		log.Print(err)
+		printUsageAndExit()
+	}
+
+	if confirm {
+		err = handleDeletions(deletionCandidates, deletionEnabled)
+		if err != nil {
+			log.Print(err)
+			printUsageAndExit()
+		}
+	}
+}
+
+func printStatusAndVerify(s []string, d []fs.FileInfo) (bool, error) {
+	fmt.Println("Protected Files:")
+	for _, ss := range s {
+		fmt.Printf("\t%s\n", ss)
+	}
+
+	fmt.Printf("\nFiles to Delete:\n")
+	for _, dd := range d {
+		fmt.Printf("\t%s\n", dd.Name())
+	}
+
+	fmt.Printf("\n %d files will be deleted. Type [y] to proceed: ", len(d))
+	reader := bufio.NewReader(os.Stdin)
+	char, _, err := reader.ReadRune()
+	if err != nil {
+		return false, fmt.Errorf("Error getting confirmation. err: %s", err)
+	}
+	if string(char) == "y" {
+		return true, nil
+	}
+	return false, nil
+}
+
+func handleDeletions(candidates []os.FileInfo, deletionEnabled bool) error {
+	for _, c := range candidates {
+		if !deletionEnabled {
+			fmt.Println("MOCK deleting ... ", c.Name())
+		}
+	}
+	return nil
+}
+
+func identifyDeletionCandidates(saveFiles []string, filesInCwd []os.FileInfo) ([]os.FileInfo, error) {
+	deletionCandidates := []os.FileInfo{}
+	// Iterate and decide
+	for _, fileInCwd := range filesInCwd {
+		fileProtected := false
+		for _, saveFile := range saveFiles {
+			if fileInCwd.Name() == saveFile {
+				fileProtected = true
+				continue
+			}
+		}
+		if !fileProtected {
+			deletionCandidates = append(deletionCandidates, fileInCwd)
+		}
+	}
+	return deletionCandidates, nil
+}
+
+func validateFile(file string) error {
+	f, err := os.Stat(file)
+	// Check that file exists
+	if err != nil {
+		return fmt.Errorf("unable to read file: %s", f)
+	}
+	// Check that file is not a directory
+	if f.IsDir() {
+		return fmt.Errorf("%s is a directory, not a plain file", f.Name())
+	}
+	return nil
 
 }
 
-func parseArgs(args []string) (string, bool) {
-	// Get the file that we're protecting
-	if len(args) < 1 || len(args) > 2 {
-		fmt.Println("usage: allbut [-f] filename")
-	}
-	force := false
-	saveFile := ""
-	for _, arg := range args {
-		if arg == "-f" {
-			force = true
-		} else {
-			saveFile = arg
-		}
-	}
-	return saveFile, force
-
-	// // Enumerate fileList
-	// fileList, err := ioutil.ReadDir("./")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// // first make sure the target file exists
-	// if !saveFileExists(saveFile, fileList) {
-	// 	fmt.Printf("fatal: file [%s] not found in current directory\n", saveFile)
-	// 	os.Exit(0)
-	// }
-
-	// // Iterate and decide
-	// for _, file := range fileList {
-	// 	if file.Name() != saveFile {
-	// 		if *force {
-	// 			e := os.Remove(file.Name())
-	// 			if e != nil {
-	// 				log.Fatal(e)
-	// 			}
-	// 		} else {
-	// 			fmt.Println("to delete:", file.Name())
-	// 		}
-	// 	} else {
-	// 		fmt.Println("protected:", file.Name())
-	// 	}
-	// }
+func printUsageAndExit() {
+	fmt.Println("usage: allbut [-f] filename")
+	os.Exit(1)
 }
 
-func saveFileExists(saveFile string, fileList []fs.FileInfo) bool {
-	for _, file := range fileList {
-		if file.Name() == saveFile {
-			return true
-		}
+func parseArgs(args []string) ([]string, bool) {
+	results := []string{}
+	deletionEnabled := false
+
+	if len(args) < 2 {
+		return []string{}, false
 	}
-	return false
+	for _, file := range args {
+		if file == "-f" {
+			deletionEnabled = true
+			continue
+		}
+		results = append(results, file)
+	}
+	return results, deletionEnabled
 }
