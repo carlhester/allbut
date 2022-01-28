93bd5f4b6a97c708916438244c4d26a6f34e33e0
Carl Hester <carlhester@gmail.com>
2022-01-27 17:09:32 -0800

    tweaks

--- main.go
+++ main.go
@@ -3,47 +3,68 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io/fs"
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
 	// Parse input
-	saveFiles, deletionEnabled := parseArgs(os.Args)
+	protectionCandidates, deleteFlagEnabled := parseArgs(os.Args)
+	if len(protectionCandidates) == 0 {
+		printUsageAndExit()
+	}
+
+	// Sanitize files
+	sanitizedFiles := sanitizeFileNames(protectionCandidates)
 
 	// validate input files
-	for _, saveFile := range saveFiles {
-		err := validateFile(saveFile)
-		if err != nil {
-			log.Printf("error validating file %s. err %v", saveFile, err)
-			printUsageAndExit()
-		}
+	protectedFiles, err := validateFiles(sanitizedFiles)
+	if err != nil {
+		log.Printf("error validating protected files: [%s]. err %v", protectedFiles, err)
+		printUsageAndExit()
 	}
 
 	// collect files from current directory
-	filesInCwd, err := ioutil.ReadDir("./")
+	cwdFiles, err := ioutil.ReadDir("./")
 	if err != nil {
 		log.Printf("error reading directory. err %v", err)
 		printUsageAndExit()
 	}
 
 	// process files
-	deletionCandidates, err := identifyDeletionCandidates(saveFiles, filesInCwd)
+	deletionCandidates, err := identifyDeletionCandidates(protectedFiles, cwdFiles)
 	if err != nil {
 		log.Print(err)
 		printUsageAndExit()
 	}
 
-	confirm, err := printStatusAndVerify(saveFiles, deletionCandidates)
+	deletionTargets := sanitizeFileNames(deletionCandidates)
+
+	// print status
+	func() {
+		fmt.Println("Protected Files:")
+		for _, s := range protectedFiles {
+			fmt.Printf("\t%s\n", s)
+		}
+
+		fmt.Printf("\nFiles to Delete:\n")
+		for _, d := range deletionTargets {
+			fmt.Printf("\t%s\n", d)
+		}
+	}()
+
+	// Get confirmation
+	deleteConfirmation, err := getDeleteConfirmation(len(deletionCandidates))
 	if err != nil {
 		log.Print(err)
 		printUsageAndExit()
 	}
 
-	if confirm {
-		err = handleDeletions(deletionCandidates, deletionEnabled)
+	// do the deed
+	if deleteConfirmation {
+		err = handleDeletions(deletionCandidates, deleteFlagEnabled)
 		if err != nil {
 			log.Print(err)
 			printUsageAndExit()
@@ -51,18 +72,20 @@ func main() {
 	}
 }
 
-func printStatusAndVerify(s []string, d []fs.FileInfo) (bool, error) {
-	fmt.Println("Protected Files:")
-	for _, ss := range s {
-		fmt.Printf("\t%s\n", ss)
+func sanitizeFileNames(files []string) []string {
+	results := []string{}
+	for _, f := range files {
+		if strings.HasPrefix(f, "./") {
+			results = append(results, f)
+		} else {
+			results = append(results, fmt.Sprintf("./%s", f))
+		}
 	}
+	return results
+}
 
-	fmt.Printf("\nFiles to Delete:\n")
-	for _, dd := range d {
-		fmt.Printf("\t%s\n", dd.Name())
-	}
-
-	fmt.Printf("\n %d files will be deleted. Type [y] to proceed: ", len(d))
+func getDeleteConfirmation(count int) (bool, error) {
+	fmt.Printf("\n%d files will be deleted. Type [y] to proceed: ", count)
 	reader := bufio.NewReader(os.Stdin)
 	char, _, err := reader.ReadRune()
 	if err != nil {
@@ -74,49 +97,52 @@ func printStatusAndVerify(s []string, d []fs.FileInfo) (bool, error) {
 	return false, nil
 }
 
-func handleDeletions(candidates []os.FileInfo, deletionEnabled bool) error {
+func handleDeletions(candidates []string, deletionEnabled bool) error {
 	for _, c := range candidates {
 		if !deletionEnabled {
-			fmt.Println("MOCK deleting ... ", c.Name())
+			fmt.Println("MOCK deleting ... ", c)
 		}
 	}
 	return nil
 }
 
-func identifyDeletionCandidates(saveFiles []string, filesInCwd []os.FileInfo) ([]os.FileInfo, error) {
-	deletionCandidates := []os.FileInfo{}
+func identifyDeletionCandidates(protectedFiles []string, filesInCwd []os.FileInfo) ([]string, error) {
+	deletionCandidates := []string{}
 	// Iterate and decide
 	for _, fileInCwd := range filesInCwd {
 		fileProtected := false
-		for _, saveFile := range saveFiles {
-			if fileInCwd.Name() == saveFile {
+		for _, protectedFile := range protectedFiles {
+			if fileInCwd.Name() == protectedFile {
 				fileProtected = true
 				continue
 			}
 		}
 		if !fileProtected {
-			deletionCandidates = append(deletionCandidates, fileInCwd)
+			deletionCandidates = append(deletionCandidates, fileInCwd.Name())
 		}
 	}
 	return deletionCandidates, nil
 }
 
-func validateFile(file string) error {
-	f, err := os.Stat(file)
-	// Check that file exists
-	if err != nil {
-		return fmt.Errorf("unable to read file: %s", f)
-	}
-	// Check that file is not a directory
-	if f.IsDir() {
-		return fmt.Errorf("%s is a directory, not a plain file", f.Name())
-	}
-	return nil
+func validateFiles(files []string) ([]string, error) {
+	for _, file := range files {
+		f, err := os.Stat(file)
 
+		// Check that file exists
+		if err != nil {
+			return []string{}, fmt.Errorf("unable to read file: %s", f)
+		}
+		// Check that file is not a directory
+		if f.IsDir() {
+			return []string{}, fmt.Errorf("%s is a directory, not a plain file", f.Name())
+		}
+	}
+	return files, nil
 }
 
 func printUsageAndExit() {
-	fmt.Println("usage: allbut [-f] filename")
+	fmt.Println("allbut deletes everything except the files you name")
+	fmt.Println("usage: allbut [-f] filename1 [filename2 filename3 ...]")
 	os.Exit(1)
 }
 
@@ -127,7 +153,11 @@ func parseArgs(args []string) ([]string, bool) {
 	if len(args) < 2 {
 		return []string{}, false
 	}
-	for _, file := range args {
+	for i, file := range args {
+		// first argument is the command name
+		if i == 0 {
+			continue
+		}
 		if file == "-f" {
 			deletionEnabled = true
 			continue
